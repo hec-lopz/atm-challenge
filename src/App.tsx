0a5a3c1eea086c12eb5168674231bcc782999226
Hector Lopez <hlopezcarballo@gmail.com>
2025-03-24 20:48:49 -0600

    fix gh-pages issues

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ function App() {
     <main className="container">
       <div className="banner">
         <div className="banner__sign-container">
-          <img className="banner__sign" src="/assets/atm_sign.png" />
-          <img className="banner__graffiti" src="/assets/graffiti.png" />
+          <img className="banner__sign" src="./assets/atm_sign.png" />
+          <img className="banner__graffiti" src="./assets/graffiti.png" />
         </div>
       </div>
       <div className="atm">
         <div className="card-selector"></div>
         <Router />
-        <img className="atm__graffiti" src="/assets/sticker_graf.png" />
+        <img className="atm__graffiti" src="./assets/sticker_graf.png" />
       </div>
     </main>
   )
